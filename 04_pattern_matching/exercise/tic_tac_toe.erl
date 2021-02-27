37de9db01aec9a62cb83a67d5cf33f8ae6b04a38
i1yas <ilyasers@gmail.com>
2021-02-27 19:36:27 +0300

    Add 04 solution

--- 04_pattern_matching/exercise/tic_tac_toe.erl
+++ 04_pattern_matching/exercise/tic_tac_toe.erl
@@ -1,17 +1,48 @@
 -module(tic_tac_toe).
 
--export([new_game/0, win/1, move/3]).
+-export([move/3, new_game/0, win/1]).
 
-
-new_game() ->
-    {{f, f, f},
-     {f, f, f},
-     {f, f, f}}.
-
+new_game() -> {{f, f, f}, {f, f, f}, {f, f, f}}.
 
 win(GameState) ->
-    no_win.
+    Result = case GameState of
+      {{X, X, X}, _, _} -> X;
+      {_, {X, X, X}, _} -> X;
+      {_, _, {X, X, X}} -> X;
+      {{X, _, _},
+      {X, _, _},
+      {X, _, _}} -> X;
+      {{_, X, _},
+      {_, X, _},
+      {_, X, _}} -> X;
+      {{_, _, X},
+      {_, _, X},
+      {_, _, X}} -> X;
+      {{X, _, _},
+      {_, X, _},
+      {_, _, X}} -> X;
+      {{_, _, X},
+      {_, X, _},
+      {X, _, _}} -> X;
+      _ -> false 
+    end,
+    case Result of
+        f -> no_win;
+        false -> no_win;
+        Win -> {win, Win}
+    end.
+        
 
-
+move(Cell, _, _) when (Cell < 1) or (Cell > 9) ->
+    {error, invalid_move};
 move(Cell, Player, GameState) ->
-    {error, invalid_move}.
+    Row = 1 + (Cell - 1) div 3,
+    Col = 1 + (Cell - 1) rem 3,
+    SelectedRow = element(Row, GameState),
+    SelectedCell = element(Col, SelectedRow),
+    case SelectedCell of
+        f -> {ok,
+                setelement(Row, GameState,
+                    setelement(Col, SelectedRow, Player))};
+        _ -> {error, invalid_move}
+    end.
--- 04_pattern_matching/exercise_hard/fizz_buzz.erl
+++ 04_pattern_matching/exercise_hard/fizz_buzz.erl
@@ -1,2 +1,13 @@
 %% Реализовать Fizz Buzz
 %% https://habr.com/ru/post/298134/
+-module(fizz_buzz).
+
+-export([fizzbuzz/1]).
+
+fizzbuzz(N) ->
+    lists:map(fun
+        (X) when (X rem 3 =:= 0) andalso (X rem 5 =:= 0) -> fizzbuzz;
+        (X) when X rem 3 =:= 0 -> fizz;
+        (X) when X rem 5 =:= 0 -> buzz;
+        (X) -> X end,
+    lists:seq(1, N)).
