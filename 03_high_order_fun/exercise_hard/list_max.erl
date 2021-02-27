d3f280fdb6f730f903e63b9ff0de758746983f7a
i1yas <ilyasers@gmail.com>
2021-02-27 17:42:36 +0300

    Add solution of 03

--- 03_high_order_fun/exercise_hard/list_max.erl
+++ 03_high_order_fun/exercise_hard/list_max.erl
@@ -1,1 +1,12 @@
 %% Реализовать функцию, которая принимает список, и возвращает максимальный элемент этого списка
+-module(list_max).
+
+-export([list_max/1]).
+
+list_max(List) ->
+    lists:foldl(fun(Item, Max) ->
+        case Item > Max of
+            true -> Item;
+            false -> Max
+        end
+    end, 0, List).
