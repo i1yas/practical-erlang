51f563212b4ba6e838c658bfacbbbd1d7ac9716d
i1yas <ilyasers@gmail.com>
2021-03-02 09:39:22 +0300

    Add 07 solution (WIP)

--- 07_binary_string/exercise/template.erl
+++ 07_binary_string/exercise/template.erl
@@ -3,4 +3,25 @@
 -export([parse/2]).
 
 parse(Str, Data) when is_binary(Str) ->
-    Str.
+    Openings = binary:matches(Str, <<"{{">>),
+    Closings = binary:matches(Str, <<"}}">>),
+    parse(Str, Data, {Openings, Closings}).
+
+parse(Str, _, {[], _}) -> Str;
+parse(Str, _, {_, []}) -> Str;
+parse(Str, Data,
+        {[Open | RestOpenings], [Close | RestClosings]}) ->
+    {TStartPos, _} = Open,
+    {TEndPos,_} = Close,
+    TLen = 2 + TEndPos - TStartPos,
+    StartPos = TStartPos + 2,
+    Len = TEndPos - TStartPos,
+    Name = binary:part(Str, StartPos, Len),
+    case Data of
+        #{Name := Replace} ->
+            NewStr = binary:replace(Str, binary:part(Str, TStartPos, TLen), Replace),
+            parse(NewStr, Data, {RestOpenings, RestClosings});
+        _ ->
+            NewStr = binary:replace(Str, binary:part(Str, TStartPos, TLen), <<"">>),
+            parse(NewStr, Data, {RestOpenings, RestClosings})
+    end.
