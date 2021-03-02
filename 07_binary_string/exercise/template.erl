030a4aa655a71997180c383b788cf38bd48e5cff
i1yas <ilyasers@gmail.com>
2021-03-02 15:06:52 +0300

    Finish 07 solution

--- 07_binary_string/exercise/template.erl
+++ 07_binary_string/exercise/template.erl
@@ -2,26 +2,34 @@
 
 -export([parse/2]).
 
-parse(Str, Data) when is_binary(Str) ->
-    Openings = binary:matches(Str, <<"{{">>),
-    Closings = binary:matches(Str, <<"}}">>),
-    parse(Str, Data, {Openings, Closings}).
+match_template(Str, start) ->
+    binary:match(Str, <<"{{">>);
+match_template(Str, finish) ->
+    binary:match(Str, <<"}}">>).
 
-parse(Str, _, {[], _}) -> Str;
-parse(Str, _, {_, []}) -> Str;
-parse(Str, Data,
-        {[Open | RestOpenings], [Close | RestClosings]}) ->
-    {TStartPos, _} = Open,
-    {TEndPos,_} = Close,
+parse(Str, Data) when is_binary(Str) ->
+    parse(Str, Data,
+        {match_template(Str, start), match_template(Str, finish)}).
+
+parse(Str, _, {nomatch, _}) -> Str;
+parse(Str, _, {_, nomatch}) -> Str;
+parse(Str, Data, {{TStartPos, _}, {TEndPos, _}}) ->
     TLen = 2 + TEndPos - TStartPos,
     StartPos = TStartPos + 2,
-    Len = TEndPos - TStartPos,
+    Len = TLen - 4,
     Name = binary:part(Str, StartPos, Len),
     case Data of
-        #{Name := Replace} ->
+        #{Name := ReplaceRaw} ->
+            Replace = if 
+                is_binary(ReplaceRaw) -> ReplaceRaw;
+                is_integer(ReplaceRaw) -> integer_to_binary(ReplaceRaw);
+                is_list(ReplaceRaw) -> unicode:characters_to_binary(ReplaceRaw)
+            end,
             NewStr = binary:replace(Str, binary:part(Str, TStartPos, TLen), Replace),
-            parse(NewStr, Data, {RestOpenings, RestClosings});
+            parse(NewStr, Data,
+                {match_template(NewStr, start), match_template(NewStr, finish)});
         _ ->
             NewStr = binary:replace(Str, binary:part(Str, TStartPos, TLen), <<"">>),
-            parse(NewStr, Data, {RestOpenings, RestClosings})
+            parse(NewStr, Data,
+                {match_template(NewStr, start), match_template(NewStr, finish)})
     end.
