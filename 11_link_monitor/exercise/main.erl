9863bc0c78d8f87d94c573c61d2811474d48a304
i1yas <ilyasers@gmail.com>
2021-03-11 22:25:28 +0300

    Done 11 solution

--- 11_link_monitor/exercise/main.erl
+++ 11_link_monitor/exercise/main.erl
@@ -1,8 +1,20 @@
 -module(main).
 
--export([parse/1]).
+-export([parse/1, parse_file/2]).
 
-accumulate({Results, Errors}) ->
+parse_file(MainPid, File) ->
+    {ok, FileContent} = file:read_file(File),
+    Result = lists:foldl(fun
+        (<<>>, Acc) -> Acc;
+        (Line, Acc) ->
+            [ _, Name, Count, _ ] = binary:split(Line, <<",">>, [global]),
+            Acc#{Name => binary_to_integer(Count)}
+        end,
+        #{},
+        binary:split(FileContent, <<"\n">>, [global])),
+    MainPid ! {result, Result}.
+
+accumulate(Workers, {Results, Errors}) ->
     receive
         {result, Result} ->
             NewResults = maps:fold(fun(Key, Value, Acc) ->
@@ -11,10 +23,19 @@ accumulate({Results, Errors}) ->
                         _ -> Acc#{Key => Value}
                     end
                 end, Results, Result),
-            {NewResults, Errors};
-        {error, File, Msg} -> {Results, Errors#{File => Msg}}
+            accumulate(Workers, {NewResults, Errors});
+        {'DOWN', _, process, _, normal} -> accumulate(Workers, {Results, Errors});
+        {'DOWN', Ref, process, Worker, Reason} ->
+            File = maps:get({Ref, Worker}, Workers),
+            accumulate(Workers, {Results, Errors#{File => Reason}})
+    after 500 ->
+        {Results, Errors}
     end.
 
 parse(Files) ->
-    [spawn_link(worker, parse, [self(), File]) || File <- Files],
-    accumulate({#{}, #{}}).
+    Workers = lists:foldl(fun(File, Acc) ->
+            Worker = spawn(?MODULE, parse_file, [self(), File]),
+            Ref = erlang:monitor(process, Worker),
+            Acc#{{Ref, Worker} => File}
+        end, #{}, Files),
+    accumulate(Workers, {#{}, #{}}).
--- 11_link_monitor/exercise/worker.erl
+++
@@ -1,8 +0,0 @@
--module(worker).
-
-parse(MainPid, File) ->
-    case file:read_file(File) of
-        {ok, FileContent} ->
-            MainPid ! {result, #{}};
-        {error, Reason} -> exit({File, Reason})
-    end.
