0337545d897138ee10bed37b70381e0772c4cbe3
i1yas <ilyasers@gmail.com>
2021-03-11 12:08:14 +0300

    Add 11 solution (WIP)

--- 11_link_monitor/exercise/main.erl
+++ 11_link_monitor/exercise/main.erl
@@ -2,6 +2,19 @@
 
 -export([parse/1]).
 
+accumulate({Results, Errors}) ->
+    receive
+        {result, Result} ->
+            NewResults = maps:fold(fun(Key, Value, Acc) ->
+                    case Acc of
+                        #{Key := AccValue} -> Acc#{Key => AccValue + Value};
+                        _ -> Acc#{Key => Value}
+                    end
+                end, Results, Result),
+            {NewResults, Errors};
+        {error, File, Msg} -> {Results, Errors#{File => Msg}}
+    end.
 
 parse(Files) ->
-    ok.
+    [spawn_link(worker, parse, [self(), File]) || File <- Files],
+    accumulate({#{}, #{}}).
--- 11_link_monitor/exercise/worker.erl
+++ 11_link_monitor/exercise/worker.erl
@@ -1,1 +1,8 @@
 -module(worker).
+
+parse(MainPid, File) ->
+    case file:read_file(File) of
+        {ok, FileContent} ->
+            MainPid ! {result, #{}};
+        {error, Reason} -> exit({File, Reason})
+    end.
