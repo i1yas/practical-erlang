0df01801ca1f6b1082fbec78fcb9c38746d2c690
i1yas <ilyasers@gmail.com>
2021-03-24 20:55:26 +0300

    Add basic 16 solution

--- 16_sockets/exercise/src/mcache.erl
+++ 16_sockets/exercise/src/mcache.erl
@@ -8,6 +8,8 @@
 
 -define(PORT, 1234).
 
+-record(state, {values}).
+
 start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
@@ -31,10 +33,24 @@ handle_connection(Id, ListenSocket, Socket) ->
             accept(Id, ListenSocket)
     end.
 
-query(Bin) ->
-    Query = binary:split(Bin, <<" ">>),
+query(RawBin) ->
+    Bin = binary:replace(RawBin, <<"\r\n">>, <<>>),
+    Query = binary:split(Bin, <<" ">>, [global]),
     case Query of
-        [<<"GET">>, Key] -> ["OK", Key];
+        [<<"GET">>, Key] ->
+            gen_server:call(?MODULE, {get, Key});
+        [<<"GETS">>, Key | Keys] ->
+            gen_server:call(?MODULE, {gets, [Key | Keys]});
+        [<<"SET">>, Key | Value ] ->
+            gen_server:call(?MODULE, {set, Key, Value});
+        [<<"DELETE">>, Key] ->
+            gen_server:call(?MODULE, {delete, Key});
+        [<<"ADD">>, Key | Value] -> 
+            gen_server:call(?MODULE, {add, Key, Value});
+        [<<"REPLACE">>, Key | Value] -> 
+            gen_server:call(?MODULE, {replace, Key, Value});
+        [<<"APPEND">>, Key | Value] -> 
+            gen_server:call(?MODULE, {append, Key, Value});
         _ -> <<"UNKNOWN REQUEST">> 
     end.
 
@@ -43,13 +59,67 @@ query(Bin) ->
 init(_Args) ->
     {ok, ListenSocket} = gen_tcp:listen(?PORT, [binary, {active, false}, {packet, line}, {reuseaddr, true}]),
     [spawn(?MODULE, accept, [Id, ListenSocket]) || Id <- lists:seq(1, 5)],
-    {ok, nostate}.
+    {ok, #state{values = #{}}}.
 
 handle_cast(_Request, State) ->
     {noreply, State}.
 
+handle_call({get, Key}, _From, State) ->
+    case State#state.values of
+        #{Key := Value} ->
+            {reply, ["VALUE ", Key, " ", Value, "\r\n", "END"], State};
+        _ -> {reply, ["NOT FOUND"], State}
+    end;
+handle_call({gets, Keys}, _From, State) ->
+    Values = lists:map(fun
+        (Key) ->
+            case State#state.values of
+                #{Key := Value} -> ["VALUE ", Key, " ", Value, "\r\n"];
+                _ -> ["VALUE ", Key, " ", "NOT FOUND\r\n"]
+            end
+        end,
+        Keys),
+    {reply, [Values, "END"], State};
+handle_call({set, Key, Value}, _From, State) ->
+    Values = State#state.values,
+    NewState = State#state{values = Values#{ Key => Value }},
+    {reply, ["STORED"], NewState};
+handle_call({delete, Key}, _From, State) ->
+    Values = State#state.values,
+    case Values of
+        #{Key := _} ->
+            NewState = State#state{values = maps:remove(Key, Values)},
+            {reply, ["DELETED"], NewState};
+        _ -> {reply, ["NOT FOUND", State]}
+    end;
+handle_call({add, Key, Value}, _From, State) ->
+    Values = State#state.values,
+    case Values of
+        #{Key := _} -> {reply, ["EXISTS"], State};
+        _ ->
+            NewState = State#state{values = Values#{ Key => Value }},
+            {reply, ["STORED"], NewState}
+    end;
+handle_call({replace, Key, Value}, _From, State) ->
+    Values = State#state.values,
+    case Values of
+        #{Key := _} ->
+            NewState = State#state{values = Values#{ Key => Value }},
+            {reply, ["STORED"], NewState};
+        _ ->
+            {reply, ["NOT FOUND"], State}
+    end;
+handle_call({append, Key, Value}, _From, State) ->
+    Values = State#state.values,
+    case Values of
+        #{Key := CurrentValue} ->
+            NewState = State#state{values = Values#{ Key => [CurrentValue, Value] }},
+            {reply, ["STORED"], NewState};
+        _ ->
+            {reply, ["NOT FOUND"], State}
+    end;
 handle_call(_Request, _From, State) ->
-    {replay, ok, State}.
+    {reply, ok, State}.
 
 handle_info(_Request, State) ->
     {noreply, State}.
